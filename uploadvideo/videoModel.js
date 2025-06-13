45789b3263f293da0ccf0135877b071bde884544
Geethuaahasolutions <geethuaahasolutions03@gmail.com>
2025-06-13 14:59:45 +0530

    solve the issue of  video

--- uploadvideo/videoModel.js
+++ uploadvideo/videoModel.js
@@ -1,33 +1,25 @@
 const db = require('../db');
 
 class VideoModel {
-  static save({ filename, language, level, path }) {
-    return new Promise((resolve, reject) => {
-      const sql = 'INSERT INTO videos (filename, language, level, path) VALUES (?, ?, ?, ?)';
-      db.query(sql, [filename, language, level, path], (err, results) => {
-        if (err) return reject(err);
-        resolve(results.insertId);
-      });
-    });
+  static async save({ filename, language, level, path }) {
+    const [results] = await db.query(
+      'INSERT INTO videos (filename, language, level, path) VALUES (?, ?, ?, ?)',
+      [filename, language, level, path]
+    );
+    return results.insertId;
   }
 
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM videos', (err, results) => {
-        if (err) return reject(err);
-        resolve(results);
-      });
-    });
+  static async getAll() {
+    const [results] = await db.query('SELECT * FROM videos');
+    return results;
   }
 
-  static getByCategory(language, level) {
-    return new Promise((resolve, reject) => {
-      const sql = 'SELECT * FROM videos WHERE language = ? AND level = ?';
-      db.query(sql, [language, level], (err, results) => {
-        if (err) return reject(err);
-        resolve(results);
-      });
-    });
+  static async getByCategory(language, level) {
+    const [results] = await db.query(
+      'SELECT * FROM videos WHERE language = ? AND level = ?',
+      [language, level]
+    );
+    return results;
   }
 }
 
