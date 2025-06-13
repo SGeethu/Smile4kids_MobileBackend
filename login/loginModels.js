b5f0da02b20a0af6404704890533536cbbcd9007
Geethuaahasolutions <geethuaahasolutions03@gmail.com>
2025-06-13 13:28:23 +0530

    change the issue of login

--- login/loginModels.js
+++ login/loginModels.js
@@ -1,14 +1,9 @@
 const db = require('../db');
 
 class LoginModel {
-  static findByEmail(email_id) {
-    return new Promise((resolve, reject) => {
-      const sql = 'SELECT * FROM users WHERE email_id = ?';
-      db.query(sql, [email_id], (err, results) => {
-        if (err) return reject(err);
-        resolve(results[0]);
-      });
-    });
+  static async findByEmail(email_id) {
+    const [results] = await db.query('SELECT * FROM users WHERE email_id = ?', [email_id]);
+    return results[0];
   }
 }
 
