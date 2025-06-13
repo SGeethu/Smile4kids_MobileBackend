d52378b0149ade1efaddc3edfee0d71789415e92
Geethuaahasolutions <geethuaahasolutions03@gmail.com>
2025-06-13 14:41:14 +0530

    live databse

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: 'mobile_backend', // Make sure this matches your DB name
-  port: process.env.DB_PORT || 3306,
+  host: 'b0nkpbgwp21n7lznkaoi-mysql.services.clever-cloud.com',
+  user: 'ue21vikyihieghjf',
+  password: 'uCKficDt4i4gQrUJiZrs',
+  database: 'b0nkpbgwp21n7lznkaoi',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
