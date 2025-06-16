88280211cc8ee542527c02270eb6c4f5599f88b4
srihari-aaha <srihariaahasolution@gmail.com>
2025-06-16 16:32:34 +0530

    local version

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const imageRoutes = require('./image/imageRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
